e8df898f376a44bdaf728eb46bbbde1129a012a0
Feric Decenan <94999837+fdecenan@users.noreply.github.com>
2025-08-24 22:33:22 +0800

    Add files via upload

---
+++ SystemVersion.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+/*
+ 
+1.8.24.2024
+Build for initial testing phase 1
+
+
+1.10.24.2024
+Build for initial testing phase 2
+
+
+1.1.22.2025
+Build for initial testing phase 3
+
+ */
+
+
+
+
+[assembly: AssemblyVersion("1.1.22.2025")]
