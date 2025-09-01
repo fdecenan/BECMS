ead1ba9bf4f1cdcdfd57675f8aba2b052662e5fb
Feric Decenan <feric.decenan.git@gmail.com>
2025-09-01 21:26:40 +0800

    yup

--- BECMS.Models/Patients/PatientModel.cs
+++ BECMS.Models/Patients/PatientModel.cs
@@ -1,4 +1,5 @@
 ﻿using FerPROJ.Design.BaseModels;
+using FerPROJ.Design.Class;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,14 +8,14 @@ using System.Threading.Tasks;
 
 namespace BECMS.Models.Patients {
     public class PatientModel : BaseModel {
-        public int Age { get; set; }
+        public int Age => Birthdate.GetAge();
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string MiddleName { get; set; } = null;
         public string Gender { get; set; }
         public string PatientStatus { get; set; }
         public string PatientAddress { get; set; }
-        public DateTime Birthdate { get; set; }
+        public DateTime Birthdate { get; set; } = DateTime.Now;
         public string ContactNumber { get; set; }
         public string EmailAddress { get; set; } = null;
         public string Occupation { get; set; }
